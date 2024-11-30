0cf4e68fc0c04a600dd5054b3a24e61d877ecf43
BOOK-6E5D09J6B9\ssalt <ssalttuck01@gmail.com>
2024-11-30 17:27:01 +0900

    기본 세팅 완료 로그인 + 회원가입 가능. mysql 연동 확인.
    - todo-
    도서 목록 페이지 프론트 작성
    데이터베이스 도서 테이블 추가 (현재는 user만 있음)

--- controllers.py
+++ controllers.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, jsonify, abort, redirect, url_for
 from flask_login import login_user, logout_user, login_required, current_user
-from models import db, User, Memo
+from models import db, User
 from login_manager import login_manager
 
 def setup_routes(app):
@@ -16,7 +16,7 @@ def setup_routes(app):
 
     @app.route('/about')
     def about():
-        return '도서 관리 시스템'
+        return '도서 대여 관리 시스템'
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
@@ -24,7 +24,7 @@ def setup_routes(app):
             user = User.query.filter_by(username=request.form['username']).first()
             if user and user.check_password(request.form['password']):
                 login_user(user)
-                return jsonify({'message': '로그인을 성공하였습니다. 메모 페이지로 이동합니다.'}), 200
+                return jsonify({'message': '로그인을 성공하였습니다. 도서 목록 페이지로 이동합니다.'}), 200
             # 에러 메시지를 JSON 형태로 반환
             return jsonify({'error': '아이디가 없거나 패스워드가 다릅니다.'}), 401
         return redirect(url_for('home'))
@@ -56,45 +56,7 @@ def setup_routes(app):
 
             return jsonify({'message': '회원가입이 성공하였습니다. 기입한 아이디와 패스워드로 로그인할 수 있습니다.'}), 201
         return redirect(url_for('home')) # 비정상요청의 경우 인 페이지로 리다이렉트
-
-    # 메모 조회
-    @app.route('/memos', methods=['GET'])
-    @login_required
-    def list_memos():
-        memos = Memo.query.filter_by(user_id=current_user.id).all()  # 현재 로그인한 사용자의 메모만 조회
-        return render_template('memos.html', memos=memos, username=current_user.username)  # 사용자별 메모를 표시하는 템플릿 렌더링
-
-    @app.route('/memos/create', methods=['POST'])
-    @login_required
-    def create_memo():
-        title = request.json['title']
-        content = request.json['content']
-        new_memo = Memo(user_id=current_user.id, title=title, content=content)  # 현재 로그인한 사용자의 ID 추가
-        db.session.add(new_memo)
-        db.session.commit()
-        return jsonify({'message': 'Memo created'}), 201
-
-    # 메모 업데이트
-    @app.route('/memos/update/<int:id>', methods=['PUT'])
-    @login_required
-    def update_memo(id):
-        memo = Memo.query.filter_by(id=id, user_id=current_user.id).first()  # 현재 사용자의 메모만 선택
-        if memo:
-            memo.title = request.json['title']
-            memo.content = request.json['content']
-            db.session.commit()
-            return jsonify({'message': 'Memo updated'}), 200
-        else:
-            abort(404, description="Memo not found or not authorized")
-
-    # 메모 삭제
-    @app.route('/memos/delete/<int:id>', methods=['DELETE'])
-    @login_required
-    def delete_memo(id):
-        memo = Memo.query.filter_by(id=id, user_id=current_user.id).first()  # 현재 사용자의 메모만 선택
-        if memo:
-            db.session.delete(memo)
-            db.session.commit()
-            return jsonify({'message': 'Memo deleted'}), 200
-        else:
-            abort(404, description="Memo not found or not authorized")
+    
+    @app.route('/library')
+    def library():
+        return '도서 목록'
--- models.py
+++ models.py
@@ -4,15 +4,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-class Memo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.String(1000), nullable=False)
-
-    def __repr__(self):
-        return f'<Memo {self.title}>'
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
